aba9d4373802e563de0b1d2df5f565fbcb01d84d
lusteiger <leonardoxy7@gmail.com>
2020-10-08 21:11:26 +0800

    [xinyu lu] implement timer function

--- src/app/Timer.js
+++ src/app/Timer.js
@@ -1,13 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Timer extends Component {
-          render() {
-                    return (
-                              <div>
-                                        Timer
-                              </div>
-                    );
-          }
+  state = {
+    time: "Start",
+  };
+
+  onTick = () => {
+    const { time } = this.state;
+    let clock = parseInt(document.getElementById("clock").value);
+    
+      timeID: setInterval(() => {
+                if(clock >0){
+                          clock--;
+                          this.setState({
+                                    time:clock +"  Seconds"
+                          })
+                }
+                else{
+                    this.setState({
+                              time:"End"
+                    })
+                }
+      }, 1000)
+    
+
+  };
+
+
+
+  render() {
+    return (
+      <div>
+        <h1>在线倒计时器</h1>
+        <div>
+          <div>
+            <label>设置时间</label>
+            <input placeholder="请设置时间" id="clock"></input>
+            <button onClick={this.onTick.bind()}>Start</button>
+          </div>
+          <div>
+            <p>{this.state.time}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Timer;
